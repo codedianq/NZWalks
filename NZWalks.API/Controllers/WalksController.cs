0902f29195cbfc42692e310229228a6855fc437a
Ian Quinto <codedianq@gmail.com>
2026-05-05 02:16:15 +0800

    Add walk controller. POST for walk.

---
+++ NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalksController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IWalkRepository walkRepository;
+
+        public WalksController(IMapper mapper, IWalkRepository walkRepository)
+        {
+            this.mapper = mapper;
+            this.walkRepository = walkRepository;
+        }
+
+        #region CREATE WALK
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddWalkRequestDto addWalkRequestDto)
+        {
+            var walkDomain = mapper.Map<Walk>(addWalkRequestDto);
+
+            walkDomain = await walkRepository.CreateAsync(walkDomain);
+
+            var walkDto = mapper.Map<WalkDto>(walkDomain);
+
+            return Ok(walkDto);
+        }
+        #endregion
+    }
+}
